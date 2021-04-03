cb7ef7a756b9d6b1e5af796f913b2eb41f0abcbc
Vandelson7593 <vandelson14@hotmail.com>
2021-04-03 01:22:18 +0100

    Firs commit

---
+++ screens/Scan.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {
+    View,
+    Text
+} from 'react-native'
+
+const Scan = () => {
+    return (
+        <View>
+            <Text>
+                Scan
+            </Text>
+        </View>
+    )
+}
+
+export default Scan
